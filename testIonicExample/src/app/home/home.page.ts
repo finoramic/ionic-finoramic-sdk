efea388bbefb07b629feba05ecba9ca5be3b043a
roshan9595 <roshanabhishek9595@gmail.com>
2019-03-05 01:16:51 +0530

    Test Ionic Example

---
+++ testIonicExample/src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+declare var FinoramicIonicPlugin: any;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+
+
+export class HomePage {
+  onClickLogin() {
+    console.log('start');
+    FinoramicIonicPlugin.initiate();
+    console.log('1');
+    FinoramicIonicPlugin.signIn();
+    console.log('end');
+    }
+}
