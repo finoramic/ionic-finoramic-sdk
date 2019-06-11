a71cd6db8fbec9a3d0b8e0d62e44f6df03cef3d8
Rajeev Desai <rajeevdesai94@gmail.com>
2019-06-11 15:41:00 +0530

    Remove old comments

--- www/FinoramicIonicPlugin.js
+++ www/FinoramicIonicPlugin.js
@@ -4,12 +4,10 @@ exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'FinoramicIonicPlugin', 'coolMethod', [arg0]);
 };
 
-// add module prefix if mutiple funciton is present else not required
 module.exports.initiate = function (arg0, arg1, success, error) {
     exec(success, error, 'FinoramicIonicPlugin', 'initiate', [arg0, arg1]);
 };
 
-// add module prefix if mutiple funciton is present else not required
 module.exports.uploadSMS = function (arg0, success, error) {
     exec(success, error, 'FinoramicIonicPlugin', 'uploadSMS', [arg0]);
 };
