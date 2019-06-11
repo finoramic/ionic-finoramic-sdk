53777fafc1bb212ff93aac062053ede080c95aed
Rajeev Desai <rajeevdesai94@gmail.com>
2019-06-11 15:40:52 +0530

    Update plugin file

--- www/FinoramicIonicPlugin.js
+++ www/FinoramicIonicPlugin.js
@@ -5,8 +5,8 @@ exports.coolMethod = function (arg0, success, error) {
 };
 
 // add module prefix if mutiple funciton is present else not required
-module.exports.initiate = function (arg0, success, error) {
-    exec(success, error, 'FinoramicIonicPlugin', 'initiate', [arg0]);
+module.exports.initiate = function (arg0, arg1, success, error) {
+    exec(success, error, 'FinoramicIonicPlugin', 'initiate', [arg0, arg1]);
 };
 
 // add module prefix if mutiple funciton is present else not required
@@ -14,6 +14,6 @@ module.exports.uploadSMS = function (arg0, success, error) {
     exec(success, error, 'FinoramicIonicPlugin', 'uploadSMS', [arg0]);
 };
 
-module.exports.getGoogleSignIn = function (arg0, arg1, arg2, arg3, success, error) {
-    exec(success, error, 'FinoramicIonicPlugin', 'getUrl', [arg0, arg1, arg2, arg3]);
+module.exports.getGoogleSignIn = function (arg0, arg1, success, error) {
+    exec(success, error, 'FinoramicIonicPlugin', 'getGoogleSignIn', [arg0, arg1]);
 }
